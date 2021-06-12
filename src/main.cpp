c9cf48b693e7724cc337378d39ceb3221ea19a1f
k.tkbys <52121121+kou-tkbys@users.noreply.github.com>
2021-06-12 15:27:02 +0900

    とりあえずカスタムクラス利用していないけど全点灯確認

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,28 @@ HT16K33 HT;
 TCA9548 MP(0x70);
 
 void setup() {
+
+  Serial.begin(9600);
+  Serial.println(__FILE__);
+  Serial.println();
+
+  Wire.begin();
+  if (MP.begin() == false)
+  {
+    Serial.println("COULD NOT CONNECT");
+  }
+  HT.begin(0x00);
   // put your setup code here, to run once:
 }
 
 void loop() {
+  uint8_t led;
+
+  Serial.println("Turn on all LEDs");
+  // first light up all LEDs
+  for (led=0; led<128; led++) {
+    HT.setLedNow(led);
+    delay(10);
+  } // for led
   // put your main code here, to run repeatedly:
 }
